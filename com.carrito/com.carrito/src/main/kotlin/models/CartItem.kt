5b6294bb2b53c8e292efb225d8e29dde887c95f3
Carlos Campos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2025-09-18 23:02:35 -0600

    Delete com.carrito/com.carrito directory

--- com.carrito/com.carrito/src/main/kotlin/models/CartItem.kt
+++
@@ -1,8 +0,0 @@
-package com.carrito.models
-
-
-// Sirve para validar cuántos de un producto se han agregado
-class CartItem(val product: Product, var quantity: Int) {
-    // Calcula el precio total del ítem basado en precio unitario y cantidad
-      fun totalPrice(): Double = product.price * quantity
-}
